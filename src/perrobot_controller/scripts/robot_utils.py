9b7291fd84f2b3aa68e97509bbbd8d7fab9c846e
Andy Mod <for.studiying@gmail.com>
2024-05-21 01:15:42 +0200

    transitions ok

--- src/perrobot_controller/scripts/robot_utils.py
+++ src/perrobot_controller/scripts/robot_utils.py
@@ -6,10 +6,15 @@ from numpy import arccos, arcsin, pi, cos, sin
 class RobotUtils:
 
     @staticmethod
-    def angles_for_height(h, L, cut=20, Rpose='x'):
+    def init_pose():
+        alpha, beta = pi/2, pi
+        return [alpha, beta, alpha, beta, -alpha, -beta, -alpha, -beta]
+
+    @staticmethod
+    def angles_for_height(h, L, cut=100, Rpose='x'):
         path = np.linspace(0, h, cut)
-        Values = [[pi/2, pi, pi/2, pi, -pi/2, -pi, -pi/2, -pi]]
-        
+        Values = [RobotUtils.init_pose()]
+
         Values.extend(RobotUtils.hight_to_angles(height, L, Rpose) for height in path)
         
         return Values
@@ -17,13 +22,13 @@ class RobotUtils:
     @staticmethod
     def hight_to_angles(h, L, Rpose='x', s=False):
         arg = 0.5 * (h / L)
-        
+
         alpha = arccos(arg)
         gamma = arcsin(arg)
         beta = gamma - alpha - pi / 2
-        
+
         a, b = round(alpha, 2), round(beta, 2)
-        
+
         if Rpose == 'x':
             values = [a, b, a, b, -a, -b, -a, -b]
         elif Rpose == 'nx':
@@ -34,14 +39,8 @@ class RobotUtils:
             values = [a, b, a, b, a, b, a, b]
         else:
             values = None
-            
+
         return (a, b) if s else values
-    
-    @staticmethod
-    def init_pose():
-        alpha, beta = pi/2, pi
-        
-        return [alpha, beta, alpha, beta, -alpha, -beta, -alpha, -beta]
 
     @staticmethod
     def angles_to_height(alpha, L):
@@ -51,15 +50,29 @@ class RobotUtils:
     def generate_sequences(a1, a2, n):
         a1 = np.array(a1)
         a2 = np.array(a2)
-        
+
         return [(1 - t) * a1 + t * a2 for t in np.linspace(0, 1, n + 1)]
+    
+    @staticmethod
+    def from_init_to_pose(h, L, target_pose, cut=100):
+        init = np.array(RobotUtils.init_pose())
+        target = np.array(RobotUtils.hight_to_angles(h, L, target_pose))
+
+        return RobotUtils.generate_sequences(init, target, cut)
 
     @staticmethod
-    def from_x_to_nx(h, L, cut=20):
-        x = np.array(RobotUtils.hight_to_angles(h, L, 'x'))
-        nx = np.array(RobotUtils.hight_to_angles(h, L, 'nx'))
-        
-        return RobotUtils.generate_sequences(x, nx, cut)
+    def from_pose_to_init(h, L, source_pose, cut=100):
+        source = np.array(RobotUtils.hight_to_angles(h, L, source_pose))
+        init = np.array(RobotUtils.init_pose())
+
+        return RobotUtils.generate_sequences(source, init, cut)
+
+    @staticmethod
+    def transition_between_poses(h, L, source_pose, target_pose, cut=100):
+        source = np.array(RobotUtils.hight_to_angles(h, L, source_pose))
+        target = np.array(RobotUtils.hight_to_angles(h, L, target_pose))
+
+        return RobotUtils.generate_sequences(source, target, cut)
 
     @staticmethod
     def rotx(alpha):
