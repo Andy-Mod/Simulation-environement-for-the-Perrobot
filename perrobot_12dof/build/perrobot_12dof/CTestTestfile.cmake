8aef13986efa83431a457d935e4c05bd3676d80f
Andy-Mod <for.studiying@gmail.co>
2024-06-04 15:54:28 +0200

    addind 12dof - work in progress

--- perrobot_12dof/build/perrobot_12dof/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/perrobot/perrobot_12dof/src/perrobot_12dof
-# Build directory: /home/perrobot/perrobot_12dof/build/perrobot_12dof
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ src/perrobot_controller/scripts/12dof/robot_controller_12dof.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import rospy
+from std_msgs.msg import Float64
+
+class RobotController:
+    RATE = 10
+
+    def __init__(self, values):
+
+        self.topics = [
+            "/perrobot_12dof_controller/FL_HFE_joint/command",
+            "/perrobot_12dof_controller/FL_KFE_joint/command",
+            "/perrobot_12dof_controller/FR_HFE_joint/command",
+            "/perrobot_12dof_controller/FR_KFE_joint/command",
+            "/perrobot_12dof_controller/HR_HFE_joint/command",
+            "/perrobot_12dof_controller/HR_KFE_joint/command",
+            "/perrobot_12dof_controller/HL_HFE_joint/command",
+            "/perrobot_12dof_controller/HL_KFE_joint/command",
+            "/perrobot_12dof_controller/HR_HAA_joint/command",
+            "/perrobot_12dof_controller/FR_HAA_joint/command",
+            "/perrobot_12dof_controller/HL_HAA_joint/command",
+            "/perrobot_12dof_controller/FL_HAA_joint/command"
+            
+        ]
+        
+        self.values = values
+        
+    def publishing_init_joint_poses(self):
+
+        publishers = [rospy.Publisher(topic, Float64, queue_size=10) for topic in self.topics]
+
+        rate = rospy.Rate(self.RATE)
+        for values in self.values:
+            for i in range(len(self.topics)):
+                message = Float64()
+                message.data = values[i]
+                publishers[i].publish(message)
+                print(f"Publishing on {self.topics[i]}: {values[i]}")
+            rate.sleep()
