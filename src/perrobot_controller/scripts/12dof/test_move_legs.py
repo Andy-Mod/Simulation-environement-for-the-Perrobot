51e4dd0dabf7569d46bb2a482d8b8539e12dbadf
Andy-Mod <for.studiying@gmail.co>
2024-06-14 15:44:43 +0200

    it works

--- src/perrobot_controller/scripts/12dof/test_move_legs.py
+++ src/perrobot_controller/scripts/12dof/test_move_legs.py
@@ -2,6 +2,7 @@
 
 from robot_publisher_12dof import RobotPublisher
 from moves12dof import Moves_12dof
+import numpy as np
 import rospy
 
 
@@ -12,10 +13,13 @@ if __name__ == '__main__':
     q2, q3 = Moves_12dof.hight_to_angles(h=Moves_12dof.TARGET_HEIGHT, L=Moves_12dof.HALF_LEG_LENGTH)
     qinit = [0.0, q2, q3]
     
-    values = Moves_12dof.move_foot(qinit, 0.1, 0.1, 2)
+    
     rate = 10
     controller = RobotPublisher(rate)
+    values = Moves_12dof.move_foot(qinit, 0.16, 0.05, 10)
+    
     
     
     controller.publishing_init_joint_poses(values, leg='FR')
+    controller.publishing_init_joint_poses(values, leg='HL')
     rospy.signal_shutdown("Done publishing")
