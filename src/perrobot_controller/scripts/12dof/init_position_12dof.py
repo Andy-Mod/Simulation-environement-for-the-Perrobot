02e56e397b9664f7f235bd9b14922fb2ee2e1768
Andy-Mod <for.studiying@gmail.co>
2024-06-20 16:50:07 +0200

    somewhat working : still some friction problems

--- src/perrobot_controller/scripts/12dof/init_position_12dof.py
+++ src/perrobot_controller/scripts/12dof/init_position_12dof.py
@@ -9,7 +9,7 @@ import rospy
 if __name__ == '__main__':
     rospy.init_node('stand_x', anonymous=True)
     values = Moves_12dof.angles_for_height(Moves_12dof.TARGET_HEIGHT, Moves_12dof.HALF_LEG_LENGTH, cut=20, Rpose='stand_x')
-    rate = 8
+    rate = 10
     controller = RobotPublisher(rate)
     
     
--- src/perrobot_controller/scripts/12dof/test_move_legs.py
+++ src/perrobot_controller/scripts/12dof/test_move_legs.py
@@ -15,15 +15,12 @@ if __name__ == '__main__':
     
     rate = 10
     controller = RobotPublisher(rate)
-    values = Moves_12dof.move_foot(qinit, 0.075, 0.075, on_x=True, dt=0.01, sub=5) # safe sub=15
-    value = Moves_12dof.move_foot(qinit, 0.075, 0.075, on_x=True, dt=0.01, sub=5, front=False)
+    values = Moves_12dof.move_foot(qinit, 0.075, 0.065, on_x=True, dt=0.01, sub=6) # safe sub=15
+    value = Moves_12dof.move_foot(qinit, 0.075, 0.055, on_x=True, dt=0.01, sub=6, front=False)
     while 1 :
         controller.publishing_init_joint_poses(values, leg='FR')
-        # rospy.sleep(0.1)
         controller.publishing_init_joint_poses(value, leg='HL')
-        # rospy.sleep(0.1)
         controller.publishing_init_joint_poses(values, leg='FL')
-        # rospy.sleep(0.1)
         controller.publishing_init_joint_poses(value, leg='HR')
     
     rospy.signal_shutdown("Done publishing")
