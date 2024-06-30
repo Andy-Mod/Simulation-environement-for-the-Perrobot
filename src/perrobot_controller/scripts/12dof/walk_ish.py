3f6908a63dee2897e7c007edaf904525560808ee
Andy-Mod <for.studiying@gmail.com>
2024-06-30 23:00:03 +0200

    redo

--- src/perrobot_controller/scripts/12dof/walk_ish.py
+++ src/perrobot_controller/scripts/12dof/walk_ish.py
@@ -8,19 +8,19 @@ import rospy
 
 
 if __name__ == '__main__':
-    # rospy.init_node('move_FR', anonymous=True)
+    rospy.init_node('move_FR', anonymous=True)
     
     q2, q3 = Moves_12dof.hight_to_angles(h=Moves_12dof.TARGET_HEIGHT, L=Moves_12dof.HALF_LEG_LENGTH)
     qinit = [0.0, q2, q3]
     
     rate = 10
     controller = RobotPublisher(rate)
-    values = Moves_12dof.move_foot(qinit, 0.075, 0.065, on_x=True, sub=100) # safe sub=6
-    value = Moves_12dof.move_foot(qinit, 0.075, 0.055, on_x=True, sub=100, front=False)
-    # while 1 :
-    #     controller.publishing_init_joint_poses(values, leg='FR')
-    #     controller.publishing_init_joint_poses(value, leg='HL')
-    #     controller.publishing_init_joint_poses(values, leg='FL')
-    #     controller.publishing_init_joint_poses(value, leg='HR')
+    values = Moves_12dof.move_foot(qinit, 0.075, 0.065, on_x=True, sub=6) # safe sub=6
+    value = Moves_12dof.move_foot(qinit, 0.075, 0.055, on_x=True, sub=6, front=False)
+    while 1 :
+        controller.publishing_init_joint_poses(values, leg='FR')
+        controller.publishing_init_joint_poses(value, leg='HL')
+        controller.publishing_init_joint_poses(values, leg='FL')
+        controller.publishing_init_joint_poses(value, leg='HR')
     
-    # rospy.signal_shutdown("Done publishing")
+    rospy.signal_shutdown("Done publishing")
